2c92f5b1ce775ddf8611014c9d5d95b95afc624f
Juanchivp12 <Juanvillegasse@gmail.com>
2024-03-24 17:22:40 -0500

    Update BattleSimulation.java

--- src/BattleSimulation.java
+++ src/BattleSimulation.java
@@ -1,10 +1,12 @@
+import java.util.Random;
 public class BattleSimulation
 {
     public static void main(String[] args) 
     {
         Character michael = new Character("michael", 4, 1);
+        Character klaus = new Character("Klaus", 3, 2);
 
-        System.out.println(michael.attack());
+
         
     }
 }
