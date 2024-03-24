f2315c2a0ac3cb79b97a3b4c14325f1aeac097c2
Juanchivp12 <Juanvillegasse@gmail.com>
2024-03-23 22:40:34 -0500

    added attack method

--- src/BattleSimulation.java
+++ src/BattleSimulation.java
@@ -2,6 +2,9 @@ public class BattleSimulation
 {
     public static void main(String[] args) 
     {
+        Character michael = new Character("michael", 4, 1);
+
+        System.out.println(michael.attack());
         
     }
 }
--- src/Character.java
+++ src/Character.java
@@ -1,13 +1,33 @@
+import java.util.Random;
 public class Character
 {
-    private int currentHealth;
+    private String name;
     private int strength;
     private int speed;
+    public final int ATTACK_DAMAGE = 10;
 
-    public Character(int currentHealth, int strength, int speed)
+    public Character(String name, int strength, int speed)
     {
-        this.currentHealth = currentHealth;
+        this.name = name;
         this.strength = strength;
         this.speed = speed;
+    }
+
+    public int attack()
+    {
+        Random rnd = new Random();
+
+        int criticalHit = rnd.nextInt(10) + 1;
+
+        if (criticalHit == 1)
+        {
+            strength += ATTACK_DAMAGE + 5;
+        }
+        else
+        {
+            strength += ATTACK_DAMAGE;
+        }
+        return strength;
+        
     }
 }
