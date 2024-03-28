5771a1452447fec49603d0dadc4404ffb3e2e058
Juanchivp12 <Juanvillegasse@gmail.com>
2024-03-28 08:26:51 -0500

    Added Comments

--- src/BattleSimulation.java
+++ src/BattleSimulation.java
@@ -8,31 +8,37 @@ public class BattleSimulation
 {
     public static void main(String[] args) 
     {
+        // Constants for battle 
         final int BOTH_ATTACK = 1;
         final int BOTH_HEAL = 2;
         final int CHAR1_ATTACKS_CHAR2_HEALS = 3;
         final int CHAR1_HEALS_CHAR2_ATTACKS = 4;
-
         final int MIN_HEALTH = 0;
         final int MIN_CHOICE = 1;
         final int MAX_CHOICE = 4;
 
+        // Symbol Strings for formatting output
         String separatedLines = "- - - - - - - - - - - - - - - - - - - - - - - - - - -";
         String dashedLines = "-----------------------------------------------------";
         String asterisks = "**************************";
 
+        // Create Random object
         Random rnd = new Random();
         
+        // Create character objects
         Character myCharacter1 = new Character("SpongeBob the square pants", 4, 1, 50);
         Character myCharacter2 = new Character("Patrick the star", 3, 2, 50);
         
+        // Output for introducing characters
         System.out.println("It's a duel to the death, let's welcome our opponents.");
         System.out.println(myCharacter1.getCharacterName() + " and " + myCharacter2.getCharacterName());
         System.out.println("Let the battle commence!");
         System.out.println(asterisks);
 
+        // While loop, stops when someone dies
         while (myCharacter1.getCharacterHealth() > MIN_HEALTH && myCharacter2.getCharacterHealth() > MIN_HEALTH)
         {
+            // Generate a random number from 1 - 4 to choose what the characters do
             int battle = rnd.nextInt(MAX_CHOICE) + MIN_CHOICE;
             
             switch (battle)
@@ -73,9 +79,11 @@ public class BattleSimulation
             }
         }
 
+        // Battle ended output
         System.out.println(asterisks);
         System.out.println("The battle has ended.");
 
+        // Outputs who won
         if (myCharacter1.getCharacterHealth() >= MIN_HEALTH)
         {
             System.out.println(myCharacter1.getCharacterName() + " has emerged victorious!");
@@ -84,5 +92,11 @@ public class BattleSimulation
         {
             System.out.println(myCharacter2.getCharacterName() + " has emerged victorious!");
         }
+    }
+
+    public static void battle()
+    {
+        
+        
     }
 }
